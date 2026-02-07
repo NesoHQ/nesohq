eef7b0609434a6121619546219a58287693cb76a
geomachine <javascriptiqbal@gmail.com>
2026-02-07 12:43:33 +0600

    Add krakens analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -35,10 +35,12 @@ export default function RootLayout({
         <Script
           src="https://krakens.nesohq.org/krakens.js"
           strategy="afterInteractive"
+          onLoad={() => {
+            if (typeof window !== 'undefined' && (window as any).Krakens) {
+              (window as any).Krakens.init('hrd_2f181a4d20e8cfa185961c53d1f5fd6ad55b53ad5f21cf66ce3478ada571f0ae');
+            }
+          }}
         />
-        <Script id="krakens-init" strategy="afterInteractive">
-          {`Krakens.init('hrd_2f181a4d20e8cfa185961c53d1f5fd6ad55b53ad5f21cf66ce3478ada571f0ae');`}
-        </Script>
         
         <ThemeProvider
           attribute="class"
